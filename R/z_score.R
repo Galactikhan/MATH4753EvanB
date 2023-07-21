6df1b8f506a0794d6354af1ce7026fc4386d12b8
Galactikhan <galactikhan@gmail.com>
2023-07-21 15:41:56 -0500

    laptop upload

--- R/z_score.R
+++ R/z_score.R
@@ -10,4 +10,15 @@
 z.score <- function(x) {
   z <- (x-mean(x))/sd(x)
   z
+  # Find the z values greater than 3 in size
+  z[abs(z)>3]
+
+  # Find the I values corresponding to these z values
+  voltage[abs(z)>3]
+
+  # Find the values of z that are possible outliers
+  z[abs(z)>=2 & abs(z)<=3]
+
+  #Find the values of I which are possible outliers
+  voltage[abs(z)>=2 & abs(z)<=3]
 }
