e029cccc1d444379a636d6730160a8bc8d690b61
Galactikhan <galactikhan@gmail.com>
2023-07-21 23:26:08 -0500

    yes

--- LAB6/lab6.R
+++ LAB6/lab6.R
@@ -1,106 +1,19 @@
-####### Task 2 Rcode #########################
-# Graphical interface calculations:
-#
-curve(dnorm(x, mean=0,sd=1),xlim=c(-3,7),ylab = "sd=1")
-# In order to find the area between x=10 and 25
-#  - seq defines desired x points
-xcurve=seq(2,5,length=1000)
-# For The Y part of the curve  we use the X points
-#  and the data for the curve in a normal distribution
-ycurve=dnorm(xcurve,mean=0,sd=1)
-# Creates a Polygon and fills it bounded by the given
-#   x & y curves
-polygon(c(2,xcurve,5),c(0,ycurve,0),col="Red")
-# Put in the text with the appropriate area
-# Area
-prob=1-pnorm(2,mean=0,sd=1)
-prob=round(prob,1)
-# Click to paste the text onto the graph
-text(locator(1), paste("Area = ", prob, sep=""))
-#
-#
-#
-curve(dnorm(x, mean=4,sd=2),xlim=c(-10,10),ylab = "sd=2")
-# In order to find the area between x=10 and 25
-#  - seq defines desired x points
-xcurve=seq(-1,5,length=1000)
-# For The Y part of the curve  we use the X points
-#  and the data for the curve in a normal distribution
-ycurve=dnorm(xcurve,mean=4,sd=2)
-# Creates a Polygon and fills it bounded by the given
-#   x & y curves
-polygon(c(-1,xcurve,5),c(0,ycurve,0),col="Red")
-# Put in the text with the appropriate area
-# Area
-prob=pnorm(5,mean=4,sd=2)-pnorm(1,mean=4,sd=2)
-prob=round(prob,2)
-# Click to paste the text onto the graph
-text(locator(1), paste("Area = ", prob, sep=""))
-#
-#
-#
-curve(dnorm(x, mean=10,sd=4),xlim=c(-10,20),ylab = "sd=2")
-#
-# In order to find the area between x=10 and 25
-#  - seq defines desired x points
-xcurve=seq(-10,10,length=1000)
-# For The Y part of the curve  we use the X points
-#  and the data for the curve in a normal distribution
-ycurve=dnorm(xcurve,mean=10,sd=4)
-# Creates a Polygon and fills it bounded by the given
-#   x & y curves
-polygon(c(-10,xcurve,10),c(0,ycurve,0),col="Red")
-# Put in the text with the appropriate area
-# Area
-prob=pnorm(10,mean=10,sd=4)
-prob=round(prob,4)
-
-# Click to paste the text onto the graph
-text(locator(1), paste("Area = ", prob, sep=""))
-#
-#
-#
-curve(dnorm(x, mean=-2,sd=1/2),xlim=c(-4,1),ylab = "sd=2")
-# In order to find the area between x=10 and 25
-#  - seq defines desired x points
-xcurve=seq(-3,-2,length=1000)
-# For The Y part of the curve  we use the X points
-#  and the data for the curve in a normal distribution
-ycurve=dnorm(xcurve,mean=-2,sd=1/2)
-# Creates a Polygon and fills it bounded by the given
-#   x & y curves
-polygon(c(-3,xcurve,-2),c(0,ycurve,0),col="Red")
-# Put in the text with the appropriate area
-# Area
-prob=pnorm(-2,mean=-2,sd=1/2)-pnorm(-3,mean=-2,sd=1/2)
-prob=round(prob,1/2)
-# Click to paste the text onto the graph
-text(locator(1), paste("Area = ", prob, sep=""))
-
-####### Task 3 Rcode ################################################
-# Gamma ##
-# Curve based of Gamma Distribution
-curve(dgamma(x,shape=3,scale=2),xlim=c(2,5),ylim=c(0,1),col="Red",lwd=2,ylab="Gamma density", main="Probabilities")
-# Text Click Paste
-text(locator(1),paste("alpha=",1))
-# Curve based of Gamma Distribution
-curve(dgamma(x,shape=6,scale=3),xlim=c(1,4),ylim=c(0,1),add=TRUE,lwd=2)
-# Text Click Paste
-text(locator(1),paste("alpha=",3))
-# Curve based of Gamma Distribution
-curve(dgamma(x,shape=2,scale=4),xlim=c(3,6),ylim=c(0,1),add=TRUE, col="Blue",lwd=2)
-# Text Click Paste
-text(locator(1),paste("alpha=",5))
-
-
-####### Task 4 Rcode ###############
-curve(dchisq(x,df=1),xlim=c(-1,10),ylim=c(0,1),col="Red",lwd=2,ylab="Chisq density", main="df=1")
-
-
-curve(dchisq(x,df=2),xlim=c(-1,10),ylim=c(0,1),col="Blue",lwd=2,ylab="Chisq density",main="df=2")
-curve(dchisq(x,df=4),xlim=c(-1,10),ylim=c(0,1),col="Green",lwd=2,ylab="Chisq density", main="df=4")
-curve(dchisq(x,df=20),xlim=c(0,40),ylim=c(0,1),col="Purple",lwd=2,ylab="Chisq density",main="df=20")
-
+#########################################
+# Code written and installed in package #
+#   for Task 7
+myncurve = function(mu, sigma, a){
+  curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma),col="purple")
+  # Create Curves
+  xcurve=seq(-100,a,length=1000)
+  ycurve=dnorm(xcurve,mean=mu,sd=sigma)
+  # Create Polygon Volume
+  polygon(c(-100,xcurve,a),c(0,ycurve,0),col="cyan")
+  # Probability
+  prob=pnorm(a,mean=mu,sd=sigma)
+  prob=round(prob,4)
+  text(x=a,y=.05, paste("Area = ", prob, sep=""))
+}
+###############################################################################
 
 
 ####################
