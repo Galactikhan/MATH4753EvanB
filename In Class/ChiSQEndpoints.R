381cac00fee9c8fa93c33bffc212e9e601582d22
Galactikhan <galactikhan@gmail.com>
2023-07-21 20:58:18 -0500

    yeetus

--- In Class/ChiSQEndpoints.R
+++ In Class/ChiSQEndpoints.R
@@ -2,11 +2,11 @@ library(Intro2R)
 ddt
 L <- ddt$LENGTH
 L
-
+n<-length(L)
 alpha <- 0.05
 
 chiL <- qchisq(1-alpha/2,n-1)
 chiU <- qchisq(alpha/2,n-1)
 
-ci <- c((n-1)*var(L)/chiL,(n-1)*var(L)/chiU)
-ci
+cinterval <- c((n-1)*var(L)/chiL,(n-1)*var(L)/chiU)
+cinterval
